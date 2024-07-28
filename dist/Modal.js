b29a16abe31e05235ce81d17966f0c22e73914d6
mystekerinos <jordy.saintalbin@gmail.com>
2024-07-28 19:44:57 +0200

    update-modal

--- dist/Modal.js
+++ dist/Modal.js
@@ -1,38 +1,45 @@
 "use strict";
 
-var _interopRequireDefault = require("/Users/jordy.saintalbin/Desktop/project/react-custom-modals/node_modules/@babel/runtime/helpers/interopRequireDefault.js").default;
+var _interopRequireWildcard = require("/Users/jordy.saintalbin/Desktop/project/react-custom-modals/node_modules/@babel/runtime/helpers/interopRequireWildcard.js").default;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _react = _interopRequireDefault(require("react"));
-require("./modal.css");
+var _react = _interopRequireWildcard(require("react"));
+require("./Modal.css");
 const Modal = _ref => {
   let {
     message,
     onClose,
-    showCloseButton = false,
-    backgroundColor = "#fff"
+    showCloseButton,
+    backgroundColor,
+    closeOnOutsideClick
   } = _ref;
-  const handleOverlayClick = e => {
-    if (e.target === e.currentTarget) {
-      onClose();
-    }
-  };
+  (0, _react.useEffect)(() => {
+    const handleOutsideClick = event => {
+      if (closeOnOutsideClick && event.target.className === "modal") {
+        onClose();
+      }
+    };
+    window.addEventListener("click", handleOutsideClick);
+    return () => {
+      window.removeEventListener("click", handleOutsideClick);
+    };
+  }, [closeOnOutsideClick, onClose]);
   return /*#__PURE__*/_react.default.createElement("div", {
-    className: "modal-overlay",
-    onClick: handleOverlayClick
-  }, /*#__PURE__*/_react.default.createElement("div", {
-    className: "modal-content",
+    className: "modal",
     style: {
-      backgroundColor
+      backgroundColor: backgroundColor || "#fff"
     }
-  }, /*#__PURE__*/_react.default.createElement("span", {
-    className: "close-button",
-    onClick: onClose
-  }, "\xD7"), /*#__PURE__*/_react.default.createElement("p", null, message), showCloseButton && /*#__PURE__*/_react.default.createElement("button", {
-    className: "close-modal-button",
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-content"
+  }, /*#__PURE__*/_react.default.createElement("p", null, message), showCloseButton && /*#__PURE__*/_react.default.createElement("button", {
     onClick: onClose
   }, "Close")));
+};
+Modal.defaultProps = {
+  showCloseButton: false,
+  backgroundColor: "#fff",
+  closeOnOutsideClick: false
 };
 var _default = exports.default = Modal;
